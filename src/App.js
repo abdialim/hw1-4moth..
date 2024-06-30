d023c221b5215e68b3d2934a45bcb56a04bf8895
islamby <islambekkaparov13@gmail.com>
2024-06-30 17:03:07 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Home";
+import Page from "./Page";
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{num}</h1>
+      <button onClick={() => {
+        if (num < 15) setNum(num + 1);
+      }}>+1</button>
+
+      <button onClick={() => {
+        if (num > 0) setNum(num - 1);
+      }}>-1</button>
+
+      <button onClick={() => {
+        setNum(0);
+      }}>reset</button>
+
+      <button onClick={() => {
+        if (num <= 10) setNum(num + 5);
+        else setNum(15);
+      }}>+5</button>
+
+      <button onClick={() => {
+        if (num >= 5) setNum(num - 5);
+        else setNum(0);
+      }}>-5</button>
     </div>
   );
 }
